0653d9f29e8813a2cae0cdde9c61f11b6e829027
Yurika Hokama <yurihoka@gmail.com>
2024-12-17 22:54:26 +0900

    Update root directory

--- weather-forecast/vite.config.ts
+++ weather-forecast/vite.config.ts
@@ -1,21 +1,25 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// });
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "tailwindcss";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  root: "src",
   plugins: [react()],
   css: {
     postcss: {
       plugins: [tailwindcss()],
+    },
+  },
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+  },
+  server: {
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      "@": "/src",
     },
   },
 });
